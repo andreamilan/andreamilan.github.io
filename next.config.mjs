52d2f91488898f9b81e2ea3e0348a2ac49d1887c
Andrea Milan <andrea.milan7@gmail.com>
2026-03-14 20:18:46 +0100

    Add animated multilingual greeting

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
-const basePath = process.env.PAGES_BASE_PATH || "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath,
-  assetPrefix: isProd && basePath ? basePath : undefined
+  }
 };
 
 export default nextConfig;
-
